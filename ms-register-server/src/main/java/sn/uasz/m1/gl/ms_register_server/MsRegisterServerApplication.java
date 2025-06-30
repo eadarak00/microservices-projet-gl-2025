c81255bd39ee751a7667b76cb5609fb81c1a7300
eadarak00 <eadarak2001@gmail.com>
2025-06-30 22:41:38 +0000

    feat(register): activation du serveur eureka

--- ms-register-server/src/main/java/sn/uasz/m1/gl/ms_register_server/MsRegisterServerApplication.java
+++ ms-register-server/src/main/java/sn/uasz/m1/gl/ms_register_server/MsRegisterServerApplication.java
@@ -2,8 +2,10 @@ package sn.uasz.m1.gl.ms_register_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsRegisterServerApplication {
 
 	public static void main(String[] args) {
