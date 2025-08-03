c10c70e28a546e90b4be5c5a4e3d7ae25b15f21d
eadarak00 <eadarak2001@gmail.com>
2025-08-03 13:56:48 +0000

    feat: les controllers

---
+++ ms-classe/src/main/java/sn/uasz/m1/gl/ms_classe/controller/ClasseRestController.java
@@ -0,0 +1,33 @@
+package sn.uasz.m1.gl.ms_classe.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+import sn.uasz.m1.gl.ms_classe.entities.Classe;
+import sn.uasz.m1.gl.ms_classe.services.ClasseService;
+
+@RestController
+@RequestMapping("/classes")
+@RequiredArgsConstructor
+public class ClasseRestController {
+
+    private final ClasseService service;
+
+    @PostMapping
+    public Classe create(@RequestBody Classe c) {
+        return service.save(c);
+    }
+
+    @PutMapping("/{id}")
+    public Classe update(@PathVariable String id, @RequestBody Classe c) {
+        return service.update(id, c);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
+}
