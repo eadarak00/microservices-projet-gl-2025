bc26edf5b2b231918cd31d2d5adf37e5d752776b
eadarak00 <eadarak2001@gmail.com>
2025-08-03 13:30:08 +0000

    feat: Repository de classe

---
+++ ms-classe/src/main/java/sn/uasz/m1/gl/ms_classe/repositories/ClasseRepository.java
@@ -0,0 +1,13 @@
+package sn.uasz.m1.gl.ms_classe.repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import sn.uasz.m1.gl.ms_classe.entities.Classe;
+
+@Repository
+public interface ClasseRepository extends MongoRepository<Classe, String> {
+    List<Classe> findByNomContainingIgnoreCase(String nom);
+}
